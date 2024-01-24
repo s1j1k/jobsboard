52acb472fd6cafa9e67229059bbe75829e75ab7d
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-01-24 18:18:04 +1100

    fetch working instead of xmlhttprequest

--- worker/tasks/fetch-jooble.js
+++ worker/tasks/fetch-jooble.js
@@ -1,30 +1,24 @@
-var XMLHttpRequest = require('xhr2');
+import('node-fetch');
 
-const url = "https://jooble.org/api/";
+let url = "https://jooble.org/api/";
 const key = "b595a9db-0c76-45b1-b48c-fc0e2ceb06d3";
-const params = "{ keywords: 'junior developer, entry level, software', location: 'Australia'}";
+url = url + key;
+const keywords = "Junior Software Developer";
+const location = "Melbourne, Australia";
+const params = "{ keywords: 'Junior Software Developer', location: 'Melbourne, Australia'}";
 
 async function fetchJooble(){
-    const res = await fetch(url);
+    //const res = await fetch(`${url}?keywords=${keywords}&location=${location}`, {method: 'POST'});
+
+    // add correct headers information
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const res = await fetch(url, {method: 'POST', headers: myHeaders, body: params});
     const jobs = await res.json();
-    console.log({jobs});
-    console.log(jobs.length);
+    //console.log(jobs);
+    console.log(jobs.length)
+
 }
 
-
-//create xmlHttpRequest object
-const http = new XMLHttpRequest();
-//open connection. true - asynchronous, false - synchronous
-http.open("POST", url + key, true);
-//Send the proper header information
-http.setRequestHeader("Content-type", "application/json");
-//Callback when the state changes
-http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) {
-        console.log(http.responseText);
-    }
-}
-//Send request to the server
-http.send(params);
-
-//fetchJooble();
+fetchJooble();

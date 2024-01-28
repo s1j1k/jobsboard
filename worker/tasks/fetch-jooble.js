cc669f911490530712ad9a559b2a6365a38019ac
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-01-28 23:07:06 +1100

    remove pagination since not working for jooble API

--- worker/tasks/fetch-jooble.js
+++ worker/tasks/fetch-jooble.js
@@ -1,24 +1,23 @@
 import('node-fetch');
-
-let url = "https://jooble.org/api/";
+const baseURL = "https://jooble.org/api/";
 const key = "b595a9db-0c76-45b1-b48c-fc0e2ceb06d3";
-url = url + key;
+const url = baseURL + key;
 const keywords = "Junior Software Developer";
 const location = "Melbourne, Australia";
 const params = "{ keywords: 'Junior Software Developer', location: 'Melbourne, Australia'}";
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
 async function fetchJooble(){
-    //const res = await fetch(`${url}?keywords=${keywords}&location=${location}`, {method: 'POST'});
 
-    // add correct headers information
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const res = await fetch(url, {method: 'POST', headers: myHeaders, body: params});
-    const jobs = await res.json();
-    //console.log(jobs);
-    console.log(jobs.length)
-
+    let resultCount = 1; //, onPage = 0;
+    const allJobs = [];
+    const res = await fetch(`${url}`, {method: 'POST', headers: myHeaders, body: params});
+    let jobs = await res.json();
+    console.log(jobs);
+    jobs = jobs["jobs"];
+    allJobs.push(...jobs);
+    console.log('got', allJobs.length, ' jobs')
 }
 
 fetchJooble();

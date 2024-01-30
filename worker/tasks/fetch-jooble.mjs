54d16c94fa280c16094242f8460497e4812c1ccf
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-01-30 14:32:16 +1100

    stringify to set redis data. fetch jooble complete

--- worker/tasks/fetch-jooble.mjs
+++ worker/tasks/fetch-jooble.mjs
@@ -11,13 +11,16 @@ const key = "b595a9db-0c76-45b1-b48c-fc0e2ceb06d3";
 const params = "{ keywords: 'Junior Software Developer', location: 'Melbourne, Australia'}";
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
+
+console.log("fetching jooble")
+
 async function fetchJooble(){
     const res = await fetch(`${baseURL}${key}`, {method: 'POST', headers: myHeaders, body: params});
     let jobs = await res.json();
     // get the array of jobs itself
     jobs = jobs["jobs"];
     console.log('got', jobs.length, ' jobs');
-    const success = await client.set("jooble", jobs);
+    const success = await client.set("jooble", JSON.stringify(jobs));
     console.log({success});
 }
 

071e58bb8ddb910adfcfb924e9e3276bfd440aec
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-01-30 19:53:25 +1100

    enable pagination for jooble api specifically

--- worker/tasks/fetch-jooble.js
+++ worker/tasks/fetch-jooble.js
@@ -8,18 +8,32 @@ const client = await createClient()
 const baseURL = "https://jooble.org/api/";
 const key = "b595a9db-0c76-45b1-b48c-fc0e2ceb06d3";
 // required for jooble API
-const params = "{ keywords: 'Junior Software Developer', location: 'Melbourne, Australia'}";
+const params = { keywords: 'Engineer, Developer, Software, Computer', location: 'Australia'};
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
 console.log("fetching jooble");
 
 export async function fetchJooble(){
-    const res = await fetch(`${baseURL}${key}`, {method: 'POST', headers: myHeaders, body: params});
-    let jobs = await res.json();
-    // get the array of jobs itself
-    jobs = jobs["jobs"];
-    console.log('got', jobs.length, ' jobs');
-    const success = await client.set("jooble", JSON.stringify(jobs));
+
+    let resultCount = 1, onPage = 1;
+    const allJobs = [];
+
+    while (resultCount > 0) {
+        params["page"] = onPage;
+        const res = await fetch(`${baseURL}${key}`, {method: 'POST', headers: myHeaders, body: JSON.stringify(params)});
+        let jobs = await res.json();
+        // get the array of jobs itself
+        jobs = jobs["jobs"];
+        allJobs.push(...jobs);
+        resultCount = jobs.length;
+        console.log('got', jobs.length, ' jobs');
+        onPage++
+    }
+    console.log('got', allJobs.length, ' jobs');
+    const success = await client.set("jooble", JSON.stringify(allJobs));
     console.log({success});
 }
+
+// testing only
+fetchJooble();

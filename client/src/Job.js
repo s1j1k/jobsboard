68ffb50de3ae214f25e38256d237b8fcb81bb23d
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-01-31 16:55:41 +1100

    add padding, date, stepper to app

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+function getDateString(date) {
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    //const daysOfWeek = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"]
+    date = new Date(date.split('T')[0])
+    //return `${daysOfWeek[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`
+    return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+}
 
 export default function Job({job}) {
+
+
     return (
-    <div className={'job'}>
-        {job.title}
-        {job.company}
-    </div>
+    <Paper className={'job'}>
+        <div>
+            <Typography variant="h6">{job.title}</Typography>
+            <Typography variant="h5">{job.company}</Typography>
+            <Typography>{job.location}</Typography>
+        </div>
+        <div>
+            <Typography>{getDateString(job.updated)}</Typography>
+        </div>
+    </Paper>
     )
 }
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 import Job from './Job';
 
 export default function Jobs({jobs}) {
+    //console.log('job is' . jobs[0]);
+    const [activeStep, setActiveStep] = React.useState(0);
+    const theme = useTheme();
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
     return (
         <div className="jobs">
-            <Typography variant="h1">
+            <Typography variant="h4" component="h1">
                 Engineering Jobs Australia
             </Typography>
             {
@@ -14,6 +28,33 @@ export default function Jobs({jobs}) {
                     job => <Job job={job} />
             )
             }
+            <MobileStepper
+                variant="progress"
+                steps={6}
+                position="static"
+                activeStep={activeStep}
+                sx={{ maxWidth: 400, flexGrow: 1 }}
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                        Next
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowLeft />
+                        ) : (
+                            <KeyboardArrowRight />
+                        )}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowRight />
+                        ) : (
+                            <KeyboardArrowLeft />
+                        )}
+                        Back
+                    </Button>
+                }
+            />
         </div>
     )
 }

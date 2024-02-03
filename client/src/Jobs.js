d2745f86180f52bfda22f5766c417a2a04ee3100
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-02-03 13:44:39 +1100

    remove "theme", just ltr, add pagination, working

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import MobileStepper from '@mui/material/MobileStepper';
 import Button from '@mui/material/Button';
@@ -7,54 +6,52 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 import Job from './Job';
+const N_JOBS_PER_PAGE = 50;
 
 export default function Jobs({jobs}) {
-    //console.log('job is' . jobs[0]);
+
+    const numJobs = jobs.length;
+    const numPages = Math.ceil(numJobs / N_JOBS_PER_PAGE);
     const [activeStep, setActiveStep] = React.useState(0);
-    const theme = useTheme();
+
+    const jobsOnPage = jobs.slice(activeStep * N_JOBS_PER_PAGE, activeStep * N_JOBS_PER_PAGE + N_JOBS_PER_PAGE);
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
     };
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
-    return (
-        <div className="jobs">
-            <Typography variant="h4" component="h1">
-                Engineering Jobs Australia
-            </Typography>
-            {
-                jobs.map(
-                    job => <Job job={job} />
-            )
+    return <div className="jobs">
+        <Typography variant="h4" component="h1">
+            Entry Level Engineering Jobs
+        </Typography>
+        <Typography variant="h6" component="h2">
+            Found {numJobs} Jobs
+        </Typography>
+        {
+            jobsOnPage.map(
+                job => <Job job={job} />
+        )
+        }
+        <div>Page {activeStep + 1} of {numPages}</div>
+        <MobileStepper
+            variant="progress"
+            steps={numPages}
+            position="static"
+            activeStep={activeStep}
+            sx={{ maxWidth: 400, flexGrow: 1 }}
+            nextButton={
+                <Button size="small" onClick={handleNext} disabled={activeStep === numPages-1}>
+                    Next
+                    <KeyboardArrowRight />
+                </Button>
             }
-            <MobileStepper
-                variant="progress"
-                steps={6}
-                position="static"
-                activeStep={activeStep}
-                sx={{ maxWidth: 400, flexGrow: 1 }}
-                nextButton={
-                    <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
-                        Next
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowLeft />
-                        ) : (
-                            <KeyboardArrowRight />
-                        )}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowRight />
-                        ) : (
-                            <KeyboardArrowLeft />
-                        )}
-                        Back
-                    </Button>
-                }
-            />
-        </div>
-    )
+            backButton={
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    <KeyboardArrowLeft />
+                    Back
+                </Button>
+            }
+        />
+    </div>
 }

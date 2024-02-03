dd1785c1b635864516f127efd9b9c0ef4caf0563
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-02-03 19:47:39 +1100

    catch fetch errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Jobs from './Jobs';
 
-const JOB_API_URL = 'http://localhost:3001/jobs'
-async function fetchJobs(updateCb: { (value: React.SetStateAction<never[]>): void; (arg0: any): void; }) {
-    const res = await fetch(JOB_API_URL);
-    const json = await res.json();
+const JOB_API_URL = 'http://localhost:3001/jobs';
 
-    updateCb(json);
+async function fetchJobs(updateCb: React.Dispatch<React.SetStateAction<any[]>>) {
+    try {
+        const res = await fetch(JOB_API_URL);
+        const json = await res.json();
+        updateCb(json);
+    } catch (error) {
+        console.error('Error fetching jobs:', error);
+        // Handle error, you might want to update the state accordingly
+    }
 }
 
 function App() {
+    const [jobList, updateJobs] = useState([]);
 
-    const [jobList, updateJobs] = React.useState([]);
-
-    React.useEffect(() => {
+    useEffect(() => {
         fetchJobs(updateJobs);
     }, []);
 
-
-  return (
-    <div className="App">
-      <Jobs jobs={jobList} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Jobs jobs={jobList} />
+        </div>
+    );
 }
 
 export default App;
